87b4c883bd2f7fd5d5748b33f7cd83a097624536
Dev Eduardo <eduardoaugustolimabueno@outlook.com.br>
2024-12-13 00:24:44 -0300

    Atualizar o YoutubePostTypes.ts

--- src/assets/types/YoutubePostTypes.ts
+++ src/assets/types/YoutubePostTypes.ts
@@ -2,3 +2,38 @@ export type YouTubePostBody = {
     youtubeVideoUrl: string,
     format: "video" | "audio",
 }
+
+export interface YouTubeVideoFormat {
+    mimeType: string;
+    qualityLabel: string | null;
+    bitrate: number | null;
+    url: string;
+    container: string;
+    codecs: string | null;
+    videoCodec?: string | null;
+    audioCodec?: string | null;
+    isLive?: boolean;
+    isHLS?: boolean;
+    isDashMPD?: boolean;
+}
+
+export interface YouTubeVideoDetails {
+    title: string;
+    duration: string;
+    description: string;
+}
+
+export interface SuccessResponse {
+    returnType: "success";
+    message: string;
+    videoFormat: YouTubeVideoFormat;
+    videoDetails: YouTubeVideoDetails;
+}
+
+export interface ErrorResponse {
+    returnType: "error";
+    message: string;
+    errorMessage?: unknown;
+}
+
+export type YouTubePostResponse = SuccessResponse | ErrorResponse;
